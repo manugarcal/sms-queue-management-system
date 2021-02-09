9c5dbae17d2cdecd819c6274fa204f7e3b7eb919
malegarcal <mgarciac@outlook.it>
2021-02-09 18:23:23 -0300

    done

--- src/main.py
+++ src/main.py
@@ -3,23 +3,19 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 """
 import os
 from flask import Flask, request, jsonify, url_for
-from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
-from admin import setup_admin
-from models import db, User
+from models import Queue
+from twilio.rest import Client
 #from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-MIGRATE = Migrate(app, db)
-db.init_app(app)
 CORS(app)
-setup_admin(app)
 
+newQueue = Queue()
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
@@ -30,14 +26,37 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
-
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
+@app.route('/new', methods=['POST'])
+def create_queue():
+    name = request.json.get('name')
+    phone = request.json.get('phone')
+    item = {
+        "name": name,
+        "phone": phone
     }
+    newQueue.enqueue(item)
 
-    return jsonify(response_body), 200
+    if not name:
+        return jsonify({"msg": "name is required"}), 400
+    if not phone:
+        return jsonify({"msg": "phone is required"}), 400
+
+    return jsonify({"Success": "user has been created"}), 201
+
+@app.route('/next', methods=['GET'])
+def next_queue():
+    if newQueue != []:
+        newQueue.dequeue()
+        return jsonify({"msg": "the list goes to the next"}),200
+    else:
+        return jsonify({"msg": "Empty List"}),400
+    
+    
+
+@app.route('/all', methods=['GET'])
+def all_queue():
+    all = newQueue.get_queue()
+    return jsonify({"success": "ok"}, all), 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,19 +1,45 @@
-from flask_sqlalchemy import SQLAlchemy
+from flask import jsonify
+from twilio.rest import Client
+import os
 
-db = SQLAlchemy()
+class Queue:
+#create a class queue and define all atributes
+    def __init__(self):
+        self.account_sid = os.environ['TWILIO_ACCOUNT_SID'] #and instance of account sid, auth token and client saved in .env fro privacy
+        self.auth_token = os.environ['TWILIO_AUTH_TOKEN']
+        self.client = Client(self.account_sid, self.auth_token)
+        self._queue = []
+        self._mode = 'FIFO'
+        
+    
+    def enqueue(self, item):
+        self._queue.append(item)
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+        message = self.client.messages \
+        .create(
+         body='Hello, '+ str(item['name'])+ ' you have ' + str(self.size()) + ' people ahead',
+         from_='+13516668915',
+         to='+56988859660'
+        )
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+        return message.sid
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+    def dequeue(self):
+        if self.size() > 0:
+            if self._mode ==  'FIFO':
+                queue = self._queue.pop(0)
+                message = self.client.messages \
+                .create(
+                body='Dear, ' + str(self._queue[0]['name']) + ' your turn has come',
+                from_='+13516668915',
+                to='+56988859660'
+                )
+                return message.sid
+        else:
+            return "empty list"
+ 
+    def get_queue(self):
+        return self._queue
+    def size(self):
+        return len(self._queue) 
+
